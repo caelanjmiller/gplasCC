90ed2a2decd039ad4d2bb778f0334cd1b9c333b6
Oscar Jordan <oscarjordan2200@live.nl>
2024-02-05 13:59:57 +0100

    deleted external shell scripts

--- gplas/scripts/module_development/1_awk_links.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-awk -F "\\t" '{{if($1 == "L") print $N}}' test_ecoli.gfa 1>> gplas_input/ecoli_raw_links.txt 2>> logs/ecoli_log_links.txt
--- gplas/scripts/module_development/4_awk_nodes_rename.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-mv gplas_input/ecoli_raw_nodes_unfiltered.fasta gplas_input/ecoli_raw_nodes.fasta
