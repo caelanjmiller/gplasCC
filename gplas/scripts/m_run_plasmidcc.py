b541493ebd26c62debbc7816fafc8dab03e254fd
Oscar Jordan <oscarjordan2200@live.nl>
2024-03-14 15:45:20 +0100

    Removed old comment

--- gplas/scripts/m_run_plasmidcc.py
+++ gplas/scripts/m_run_plasmidcc.py
@@ -8,7 +8,7 @@ def run_plasmidCC(infile, sample, species, maxlen):
         cmd = f"plasmidCC -i {infile} -o plasmidCC -n {sample} -s {species} -l {maxlen} -D -g -f"
         try:
             subprocess.run(cmd, shell=True, check=True)
-        except subprocess.CalledProcessError as e: #TODO improve this error handling; it does not catch an error in argument parsing (wrong -s name)
+        except subprocess.CalledProcessError as e:
             print("\n")
             print("plasmidCC has run into an unexpected error!")
             raise Exception(e.returncode)
